75aa26842fee4d5df6988d180546cfbfd3b8cfa3
Renan Oliveira Netto <renan.o.netto@gmail.com>
2016-09-25 17:30:36 -0300

    added documentation for geometry library

--- ophidian/geometry/Distance.h
+++ ophidian/geometry/Distance.h
@@ -8,19 +8,41 @@ namespace geometry {
 
 class ManhattanDistance {
 public:
+    //! Construct ManhattanDistance
+    /*!
+     * \brief Empty default constructor
+     */
 	ManhattanDistance() {
 
 	}
 
+    //! Distance operator
+    /*!
+     * \brief Calculates the Manhattan distance between two points
+     * \param point1 First point to calculate the distance
+     * \param point2 Second point to calculate the distance
+     * \return Manhattan distance between point1 and point2
+     */
     double operator()(const Point & point1, const Point & point2);
 };
 
 class EuclideanDistance {
 public:
+    //! Construct EuclideanDistance
+    /*!
+     * \brief Empty default constructor
+     */
 	EuclideanDistance() {
 
 	}
 
+    //! Distance operator
+    /*!
+     * \brief Calculates the Euclidean distance between two points
+     * \param point1 First point to calculate the distance
+     * \param point2 Second point to calculate the distance
+     * \return Euclidean distance between point1 and point2
+     */
     double operator()(const Point &point1, const Point &point2);
 };
 
--- ophidian/geometry/Models.h
+++ ophidian/geometry/Models.h
@@ -19,6 +19,12 @@ using Box = boost::geometry::model::box<Point>;
 using Polygon = boost::geometry::model::polygon<Point>;
 using MultiPolygon = boost::geometry::model::multi_polygon<Polygon>;
 
+//! Create new geometry
+/*!
+ *  \brief Creates a new geometry from a vector of points. Can be used for Linestring and Polygon. Should not be used for Segment and Box.
+ * \param points Vector of points of the geometry
+ * \return The created geometry
+ */
 template<class Geometry>
 Geometry make(const std::vector<Point> & points) {
     Geometry geometry;
@@ -28,6 +34,12 @@ Geometry make(const std::vector<Point> & points) {
     return geometry;
 }
 
+//! Create new multi geometry
+/*!
+ *  \brief Creates a new multi geometry from a vector of points. Can be used for MultiPolygon, but could be extended for other multi geometries, such as a MultiBox.
+ * \param parts The geometries of the multi geometry
+ * \return The created multi geometry
+ */
 template<class MultiGeometry, class PartGeometry>
 MultiGeometry makeMulti(const std::vector<PartGeometry> & parts) {
     MultiGeometry multiGeometry;
