f5188b4f79c1c90c859643864e6460536ee8c295
bernardo <bernardo.mferrari@gmail.com>
2016-09-29 17:14:40 -0300

    added errorMessage

--- ophidian/parser/ParserException.h
+++ ophidian/parser/ParserException.h
@@ -1,10 +1,14 @@
 #ifndef PARSER_EXCEPTION_H
 #define PARSER_EXCEPTION_H
 
+#include <string>
+
 namespace ophidian {
     namespace parser {
 
         class InexistentFile {
+            std::string errorMessage(){
+                return "The given file was not found";
         };
 
     }
