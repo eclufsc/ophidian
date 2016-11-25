a8667fc72a26b4ee73ab977357ea38f3d4e8d09d
Sheiny Fabre <sheiny.fabre@gmail.com>
2016-11-25 11:44:10 -0200

    Added a missing CMakeLists file and Core/Threadpool as a Test

--- test/core/threadpooltest.cpp
+++ test/core/threadpooltest.cpp
@@ -1,101 +1,10 @@
+#include <catch.hpp>
 #include "threadpooltest.h"
-
-#include <queue>
-#include <thread>
-#include <mutex>
-#include <atomic>
-#include <memory>
-#include <array>
-#include <algorithm>
-#include <chrono>
-#include <cmath>
-#include <iostream>
-#include <random>
-
-template <uint32_t kNumThreads>
-class ThreadPool {
-    using QueueOfTasks = std::queue< std::function<void()> >;
-public:
-    ThreadPool() :
-    stop_(false) {
-        workers_.reserve(kNumThreads);
-        for(uint32_t i = 0; i < kNumThreads; ++i) {
-            workers_.emplace_back([this]{
-                std::function<void()> task;
-                for(;;) {
-                    {
-                        std::unique_lock<std::mutex> lock(queueMutex_);
-                        condition_.wait(lock, [this]()->bool{
-                            return stop_ || !tasks_.empty();
-                        });
-                        if(stop_ && tasks_.empty())
-                        {
-                            return;
-                        }
-                        task = std::move(tasks_.front());
-                        tasks_.pop();
-                    }
-                    task();
-                }
-            });
-        }
-    }
-
-    ~ThreadPool() {
-        {
-            std::unique_lock<std::mutex> lock(queueMutex_);
-            stop_ = true;
-        }
-        condition_.notify_all();
-        for(auto & worker : workers_)
-        {
-            worker.join();
-        }
-    }
-
-    template <class T>
-    void post(T && task) {
-        {
-            {
-                std::unique_lock<std::mutex> lock(queueMutex_);
-                tasks_.emplace([task](){
-                    task();
-                });
-            }
-            condition_.notify_one();
-        }
-    }
-private:
-    std::vector<std::thread> workers_;
-    QueueOfTasks tasks_;
-    std::mutex queueMutex_;
-    std::condition_variable condition_;
-    bool stop_;
-};
-
-
-template <uint32_t kLutDimension>
-struct LutIndices {
-    std::array<double, 500> references;
-    std::array<std::array<double, kLutDimension>, 500> values;
-    typename std::array<typename std::array<double, kLutDimension>::size_type, 500> indices;
-
-    LutIndices() {
-        for(auto& bounds : values) {
-            double curr = 0.0;
-            for(auto & value : bounds) {
-                value = std::pow(2.0, curr);
-                curr += 1.0;
-            }
-        }
-    }
-};
 
 std::condition_variable condition;
 std::mutex ready_m;
 
-
-int main() {
+TEST_CASE("Thread Pool Test", "[thread]") {
     const auto kLutSize = 7;
     std::default_random_engine generator;
     auto pool = std::make_unique<ThreadPool<4>>();
@@ -152,6 +61,4 @@ int main() {
         }
         condition.notify_all();
     });
-
-    return 0;
 }
--- test/core/threadpooltest.h
+++ test/core/threadpooltest.h
@@ -1,5 +1,97 @@
 #ifndef OPHIDIAN_CORE_THREAD_POOL_TEST_H
 #define OPHIDIAN_CORE_THREAD_POOL_TEST_H
 
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <atomic>
+#include <memory>
+#include <array>
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <iostream>
+#include <random>
+#include <condition_variable>
+
+template <uint32_t kNumThreads>
+class ThreadPool {
+    using QueueOfTasks = std::queue< std::function<void()> >;
+public:
+    ThreadPool() :
+    stop_(false) {
+        workers_.reserve(kNumThreads);
+        for(uint32_t i = 0; i < kNumThreads; ++i) {
+            workers_.emplace_back([this]{
+                std::function<void()> task;
+                for(;;) {
+                    {
+                        std::unique_lock<std::mutex> lock(queueMutex_);
+                        condition_.wait(lock, [this]()->bool{
+                            return stop_ || !tasks_.empty();
+                        });
+                        if(stop_ && tasks_.empty())
+                        {
+                            return;
+                        }
+                        task = std::move(tasks_.front());
+                        tasks_.pop();
+                    }
+                    task();
+                }
+            });
+        }
+    }
+
+    ~ThreadPool() {
+        {
+            std::unique_lock<std::mutex> lock(queueMutex_);
+            stop_ = true;
+        }
+        condition_.notify_all();
+        for(auto & worker : workers_)
+        {
+            worker.join();
+        }
+    }
+
+    template <class T>
+    void post(T && task) {
+        {
+            {
+                std::unique_lock<std::mutex> lock(queueMutex_);
+                tasks_.emplace([task](){
+                    task();
+                });
+            }
+            condition_.notify_one();
+        }
+    }
+private:
+    std::vector<std::thread> workers_;
+    QueueOfTasks tasks_;
+    std::mutex queueMutex_;
+    std::condition_variable condition_;
+    bool stop_;
+};
+
+
+template <uint32_t kLutDimension>
+struct LutIndices {
+    std::array<double, 500> references;
+    std::array<std::array<double, kLutDimension>, 500> values;
+    typename std::array<typename std::array<double, kLutDimension>::size_type, 500> indices;
+
+    LutIndices() {
+        for(auto& bounds : values) {
+            double curr = 0.0;
+            for(auto & value : bounds) {
+                value = std::pow(2.0, curr);
+                curr += 1.0;
+            }
+        }
+    }
+};
+
 
 #endif // OPHIDIAN_CORE_THREAD_POOL_TEST_H
