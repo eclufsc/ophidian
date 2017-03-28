d1cb8e9c193cd6615fc4dae090d21b403268b40a
Tiago Augusto Fontana <tiagoaugustofontana@gmail.com>
2017-03-28 14:00:14 -0300

    Implemented and tested of kmeans in DOD non parallel.

---
+++ test/sbcci2017/kmeansdataorienteddesign.h
@@ -0,0 +1,11 @@
+#ifndef KMEANSDATAORIENTEDDESIGN_H
+#define KMEANSDATAORIENTEDDESIGN_H
+
+
+class KmeansDataOrientedDesign
+{
+public:
+    KmeansDataOrientedDesign();
+};
+
+#endif // KMEANSDATAORIENTEDDESIGN_H
