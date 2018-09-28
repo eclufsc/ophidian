43e1e5438ab6990b4c2349e80967907014056165
tarberd <bernardo.mferrari@gmail.com>
2018-09-28 17:53:02 -0300

    Test box scaling

--- test/geometry/scaling_test.cpp
+++ test/geometry/scaling_test.cpp
@@ -16,16 +16,16 @@ TEST_CASE("Geometry: scaling a Point", "[geometry][operations]")
     CHECK(scaledPoint.x() == Approx(2.0));
     CHECK(scaledPoint.y() == Approx(4.0));
 }
-//
-// TEST_CASE("Geometry: scaling a Box", "[geometry][operations]") {
-//     Point scalingPoint(2.0, 2.0);
-//
-//     BoxFixture boxFixture;
-//
-//     Box scaledBox = scale(boxFixture.box, scalingPoint);
-//
-//     REQUIRE(scaledBox.min_corner().x() == boxFixture.xMin * scalingPoint.x());
-//     REQUIRE(scaledBox.min_corner().y() == boxFixture.yMin * scalingPoint.y());
-//     REQUIRE(scaledBox.max_corner().x() == boxFixture.xMax * scalingPoint.x());
-//     REQUIRE(scaledBox.max_corner().y() == boxFixture.yMax * scalingPoint.y());
-// }
+
+TEST_CASE("Geometry: scaling a Box", "[geometry][operations]") {
+    auto scalingPoint = Point<double>{2.0, 2.0};
+
+    auto box = Box<double>{{1.0, 2.0}, {2.0, 2.0}};
+
+    auto scaledBox = scale(box, scalingPoint);
+
+    REQUIRE(scaledBox.min_corner().x() == Approx(2.0));
+    REQUIRE(scaledBox.min_corner().y() == Approx(4.0));
+    REQUIRE(scaledBox.max_corner().x() == Approx(4.0));
+    REQUIRE(scaledBox.max_corner().y() == Approx(4.0));
+}
