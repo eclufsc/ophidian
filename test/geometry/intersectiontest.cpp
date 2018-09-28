f389e5e0b0da281568b58cb4eb379852e9c88269
tarberd <bernardo.mferrari@gmail.com>
2018-09-28 14:58:45 -0300

    Refactor interconection test

--- test/geometry/intersectiontest.cpp
+++ test/geometry/intersectiontest.cpp
@@ -1,55 +1,41 @@
 #include <catch.hpp>
 
+#include <ophidian/util/Units.h>
+#include <ophidian/geometry/Models.h>
 #include <ophidian/geometry/Operations.h>
 
 using namespace ophidian::geometry;
 
 TEST_CASE("Geometry: intersection between two boxes", "[geometry][operations]") {
-    Box<double> box1({1.0, 1.0}, {3.0, 3.0});
-    Box<double> box2({2.0, 1.0}, {4.0, 3.0});
+    SECTION("Intersection betwen box of double")
+    {
+        auto box1 = Box<double>{{1.0, 1.0}, {3.0, 3.0}};
+        auto box2 = Box<double>{{2.0, 1.0}, {4.0, 3.0}};
 
-    Box<double> intersectionBox;
-    intersection(box1, box2, intersectionBox);
+        auto result_box = Box<double>{};
 
-    REQUIRE(intersectionBox.min_corner().x() == Approx(2.0));
-    REQUIRE(intersectionBox.min_corner().y() == Approx(1.0));
-    REQUIRE(intersectionBox.max_corner().x() == Approx(3.0));
-    REQUIRE(intersectionBox.max_corner().y() == Approx(3.0));
+        intersection(box1, box2, result_box);
+
+        REQUIRE(result_box.min_corner().x() == Approx(2.0));
+        REQUIRE(result_box.min_corner().y() == Approx(1.0));
+        REQUIRE(result_box.max_corner().x() == Approx(3.0));
+        REQUIRE(result_box.max_corner().y() == Approx(3.0));
+    }
+
+    SECTION("Intersection betwen box of database_unit_t")
+    {
+        using dbu = ophidian::util::database_unit_t;
+
+        auto box1 = Box<dbu>{{dbu{1.0}, dbu{1.0}}, {dbu{3.0}, dbu{3.0}}};
+        auto box2 = Box<dbu>{{dbu{2.0}, dbu{1.0}}, {dbu{4.0}, dbu{3.0}}};
+
+        auto result_box = Box<dbu>{};
+
+        intersection(box1, box2, result_box);
+
+        REQUIRE(result_box.min_corner().x() == dbu{2.0});
+        REQUIRE(result_box.min_corner().y() == dbu{1.0});
+        REQUIRE(result_box.max_corner().x() == dbu{3.0});
+        REQUIRE(result_box.max_corner().y() == dbu{3.0});
+    }
 }
-
-// TEST_CASE("Geometry: intersection between two polygons", "[geometry][operations]") {
-//     std::vector<Point<double>> points1 = {
-//         Point<double>{1.0, 1.0},
-//         Point<double>{1.0, 3.0},
-//         Point<double>{3.0, 3.0},
-//         Point<double>{3.0, 1.0},
-//         Point<double>{1.0, 1.0},
-//     };
-//     auto polygon1 = Polygon<double>{points1};
-//
-//     std::vector<Point<double>> points2 = {
-//         Point<double>{2.0, 1.0},
-//         Point<double>{2.0, 3.0},
-//         Point<double>{4.0, 3.0},
-//         Point<double>{4.0, 1.0},
-//         Point<double>{2.0, 1.0},
-//     };
-//     auto polygon2 = Polygon<double>{points2};
-//
-//     MultiPolygon<double> intersectionResult;
-//     intersection(polygon1, polygon2, intersectionResult);
-//
-//     REQUIRE(intersectionResult.size() == 1);
-//
-//     std::vector<Point<double>> intersectionPoints(intersectionResult.at(0).outer().begin(), intersectionResult.at(0).outer().end());
-//     REQUIRE(intersectionPoints.at(0).x() == Approx(2.0));
-//     REQUIRE(intersectionPoints.at(0).y() == Approx(3.0));
-//     REQUIRE(intersectionPoints.at(1).x() == Approx(3.0));
-//     REQUIRE(intersectionPoints.at(1).y() == Approx(3.0));
-//     REQUIRE(intersectionPoints.at(2).x() == Approx(3.0));
-//     REQUIRE(intersectionPoints.at(2).y() == Approx(1.0));
-//     REQUIRE(intersectionPoints.at(3).x() == Approx(2.0));
-//     REQUIRE(intersectionPoints.at(3).y() == Approx(1.0));
-//     REQUIRE(intersectionPoints.at(4).x() == Approx(2.0));
-//     REQUIRE(intersectionPoints.at(4).y() == Approx(3.0));
-// }
