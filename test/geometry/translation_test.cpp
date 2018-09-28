171f9a15d4da130954276609bb9057fefc33484c
tarberd <bernardo.mferrari@gmail.com>
2018-09-28 18:05:52 -0300

    Test Box translate

--- test/geometry/translation_test.cpp
+++ test/geometry/translation_test.cpp
@@ -15,16 +15,19 @@ TEST_CASE("Geometry: translating a Point", "[geometry][operations]") {
     CHECK(translatedPoint.x() == Approx(2.0));
     CHECK(translatedPoint.y() == Approx(3.0));
 }
-//
-// TEST_CASE("Geometry: translating a Box", "[geometry][operations]") {
-//     Point translationPoint(1.0, 1.0);
-//
-//     BoxFixture boxFixture;
-//
-//     Box translatedBox = translate(boxFixture.box, translationPoint);
-//
-//     REQUIRE(translatedBox.min_corner().x() == boxFixture.xMin + translationPoint.x());
-//     REQUIRE(translatedBox.min_corner().y() == boxFixture.yMin + translationPoint.y());
-//     REQUIRE(translatedBox.max_corner().x() == boxFixture.xMax + translationPoint.x());
-//     REQUIRE(translatedBox.max_corner().y() == boxFixture.yMax + translationPoint.y());
-// }
+
+TEST_CASE("Geometry: translating a Box", "[geometry][operations]") {
+    auto translationPoint = Point<double>{1.0, 1.0};
+
+    auto point = Box<double>{
+        {1.0, 2.0},
+        {2.0, 2.0}
+    };
+
+    auto translatedBox = translate(point, translationPoint);
+
+    REQUIRE(translatedBox.min_corner().x() == Approx(2.0));
+    REQUIRE(translatedBox.min_corner().y() == Approx(3.0));
+    REQUIRE(translatedBox.max_corner().x() == Approx(3.0));
+    REQUIRE(translatedBox.max_corner().y() == Approx(3.0));
+}
