77ac76339b90b42c6eae7bfe9837287b4ee2df6e
tarberd <bernardo.mferrari@gmail.com>
2018-09-28 14:06:20 -0300

    refactor distance test

--- test/geometry/distance_test.cpp
+++ test/geometry/distance_test.cpp
@@ -1,21 +1,59 @@
 #include <catch.hpp>
 
 #include <ophidian/geometry/Distance.h>
+#include <ophidian/util/Units.h>
 
 using namespace ophidian::geometry;
 
-TEST_CASE("Geometry: manhattan distance between two points", "[geometry][distance]") {
-    Point<double> point1(5, 10);
-    Point<double> point2(3, 7);
-    double distance = ManhattanDistance(point1, point2);
-    double expected_distance = 5;
-    REQUIRE(distance == Approx(expected_distance));
+template <typename T>
+void test_manhattan_distance(T&& x1, T&& y1, T&& x2, T&& y2, T&& result)
+{
+    auto point_1 = Point<T>(x1, y1);
+    auto point_2 = Point<T>(x2, y2);
+
+    CHECK(ManhattanDistance(point_1, point_2) == result);
 }
 
-TEST_CASE("Geometry: euclidean distance between two points", "[geometry][distance]") {
-    Point<double> point1(5, 10);
-    Point<double> point2(3, 7);
-    double distance = EuclideanDistance(point1, point2);
-    double expected_distance = std::sqrt(13);
-    REQUIRE(distance == Approx(expected_distance));
+TEST_CASE("Geometry: manhattan distance between two points", "[geometry][distance]")
+{
+    SECTION("Test distance from points of double")
+    {
+        test_manhattan_distance(0.0d, 0.0d, 0.0d, 0.0d, 0.0d);
+
+        test_manhattan_distance(5.0d, 10.0d, 3.0d, 7.0d, 5.0d);
+
+        test_manhattan_distance(-5.0d, -10.0d, -3.0d, -7.0d, 5.0d);
+    }
+
+    SECTION("Test distance from points of database_unit_t")
+    {
+        using dbu = ophidian::util::database_unit_t;
+
+        test_manhattan_distance(dbu{0}, dbu{0}, dbu{0}, dbu{0}, dbu{0});
+
+        test_manhattan_distance(dbu{5}, dbu{10}, dbu{3}, dbu{7}, dbu{5});
+
+        test_manhattan_distance(dbu{-5}, dbu{-10}, dbu{-3}, dbu{-7}, dbu{5});
+    }
+}
+
+template <typename T>
+void test_euclidean_distance(T&& x1, T&& y1, T&& x2, T&& y2, T&& result)
+{
+    auto point_1 = Point<T>(x1, y1);
+    auto point_2 = Point<T>(x2, y2);
+
+    CHECK(EuclideanDistance(point_1, point_2) == result);
+}
+
+TEST_CASE("Geometry: euclidean distance between two points", "[geometry][distance]")
+{
+    SECTION("Test distance from points of double")
+    {
+        test_euclidean_distance(0.0d, 0.0d, 0.0d, 0.0d, 0.0d);
+
+        test_euclidean_distance(5.0d, 10.0d, 3.0d, 7.0d, std::sqrt(13));
+
+        test_euclidean_distance(-5.0d, -10.0d, -3.0d, -7.0d, std::sqrt(13));
+    }
 }
