ad508a70b3d21321da761c4ec02e9634ff2d0236
Renan Oliveira Netto <renan.o.netto@gmail.com>
2018-10-23 14:35:58 -0300

    Fix cells in factory
    This commit updates the PlacementFactory to fix the cells correctly

--- test/placement/placement_factory_test.cpp
+++ test/placement/placement_factory_test.cpp
@@ -30,6 +30,7 @@ TEST_CASE("Placement factory: populate with simple project.", "[placement][Place
     ophidian::placement::factory::make_placement(placement, def, netlist);
 
     auto cell_u1 = netlist.find_cell_instance("u1");
+    auto cell_f1 = netlist.find_cell_instance("f1");
 
     using dbu_t = ophidian::placement::Placement::unit_type;
 
@@ -38,6 +39,9 @@ TEST_CASE("Placement factory: populate with simple project.", "[placement][Place
 
     CHECK(cell_u1_xpos == dbu_t{3420});
     CHECK(cell_u1_ypos == dbu_t{6840});
+
+    CHECK(!placement.fixed(cell_u1));
+    CHECK(placement.fixed(cell_f1));
 
     auto std_cell_u1 = netlist.std_cell(cell_u1);
 
