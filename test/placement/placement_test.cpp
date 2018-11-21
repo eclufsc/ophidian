51de1172dea45486fe5cdd194cbfe921d7db3238
Renan Oliveira Netto <renan.o.netto@gmail.com>
2018-11-21 10:30:29 -0200

    Add tests for cell geometry and pin location
    This commit adds code to the pin location test to also test
    the location of pins that belong to a cell.
    Also, it adds a test to get the geometry of a cell before and
    after it is placed.

--- test/placement/placement_test.cpp
+++ test/placement/placement_test.cpp
@@ -7,8 +7,14 @@
 using namespace ophidian::placement;
 using namespace ophidian::circuit;
 
+using dbu_type = Library::std_cell_geometry_type::unit_type;
 class NetlistFixture {
 public:
+    Cell stdCell1;
+
+    Pin pinType1;
+    Pin pinType2;
+
     CellInstance cell1, cell2;
 
     PinInstance pin1, pin2, pin3, pin4, pin5, pin6;
@@ -17,12 +23,29 @@ public:
 
     Output output1, output2;
 
+    StandardCells stdCells;
     Netlist netlist;
-    Library library{StandardCells{}};
+    Library library{stdCells};
 
     NetlistFixture() {
+        stdCell1 = stdCells.add_cell("invX1");
+        pinType1 = stdCells.add_pin("invX1:a", PinDirection::INPUT);
+        pinType2 = stdCells.add_pin("invX1:o", PinDirection::OUTPUT);
+
+        auto cellBox = Library::std_cell_geometry_type::box_type{{dbu_type{0}, dbu_type{0}}, 
+        {dbu_type{10}, dbu_type{10}}};
+        auto cellGeometry = Library::std_cell_geometry_type{{cellBox}};
+
+        library.geometry(stdCell1) = cellGeometry;
+
+        library.offset(pinType1) = {dbu_type{1}, dbu_type{2}};
+        library.offset(pinType2) = {dbu_type{2}, dbu_type{1}};
+
         cell1 = netlist.add_cell_instance("cell1");
         cell2 = netlist.add_cell_instance("cell2");
+
+        netlist.connect(cell1, stdCell1);
+        netlist.connect(cell2, stdCell1);
 
         pin1 = netlist.add_pin_instance("pin1");
         pin2 = netlist.add_pin_instance("pin2");
@@ -30,6 +53,9 @@ public:
         pin4 = netlist.add_pin_instance("pin4");
         pin5 = netlist.add_pin_instance("pin5");
         pin6 = netlist.add_pin_instance("pin6");
+
+        netlist.connect(pin5, pinType1);
+        netlist.connect(pin6, pinType2);
 
         input1 = netlist.add_input_pad(pin1);
         input2 = netlist.add_input_pad(pin2);
@@ -132,4 +158,23 @@ TEST_CASE_METHOD(NetlistFixture, "Placement: placing an output pad", "[placement
 
     REQUIRE(placement.location(output1).x() != placement.location(output2).x());
     REQUIRE(placement.location(output1).y() != placement.location(output2).y());
+}
+
+TEST_CASE_METHOD(NetlistFixture, "Placement: get cell geometry", "[placement]") {
+    auto placement = Placement{netlist, library};
+
+    auto cellBox = Library::std_cell_geometry_type::box_type{{dbu_type{0}, dbu_type{0}}, 
+        {dbu_type{10}, dbu_type{10}}};
+    auto cellGeometry = Placement::cell_geometry_type{ {cellBox} };
+
+    REQUIRE(placement.geometry(cell1) == cellGeometry);
+    REQUIRE(placement.geometry(cell2) == cellGeometry);
+
+    placement.place(cell1, {dbu_type{10}, dbu_type{20}});
+
+    auto newCellBox = Library::std_cell_geometry_type::box_type{{dbu_type{10}, dbu_type{20}}, 
+        {dbu_type{20}, dbu_type{30}}};
+    auto newCellGeometry = Placement::cell_geometry_type{ {newCellBox} };
+
+    REQUIRE(placement.geometry(cell1) == newCellGeometry);
 }
