def8194a61b4333398b34e04128b3a692fac4cc4
Tiago Fontana <tiagoaugustofontana@gmail.com>
2020-03-04 21:23:04 -0300

    Creating tests for Gcell Graph.

--- test/routing/gcell_graph_test.cpp
+++ test/routing/gcell_graph_test.cpp
@@ -15,6 +15,25 @@ bool boxCompare(const box_type& a, const box_type& b)
 
 TEST_CASE("GCell Graph Test", "[routing][gcell]")
 {
+    /*
+       Y
+       |
+    850|----|-------|-------|-------|----|   
+       |    |       |       |       |    |   
+    700|----|-------|-------|-------|----|   
+       |    |       |       |       |    |   
+       |    |       | gcell |       |    |   
+    500|----|-------|-------|-------|----|   
+       |    |       |       |       |    |   
+       |    |       |       |       |    |   
+    300|----|-------|-------|-------|----|   
+       |    |       |       |       |    |   
+       |    |       |       |       |    |   
+    100|----|-------|-------|-------|----|   
+       |    |       |       |       |    |     
+      0---------------------------------------- X
+       0   200     500     800    1100  1150
+    */
     std::vector<dbu> x = {dbu(0), dbu(200), dbu(500), dbu(800), dbu(1100), dbu(1150)};
     std::vector<dbu> y = {dbu(0), dbu(100), dbu(300), dbu(500), dbu(700), dbu(850)};
     std::vector<int> capacity = {2,4,6};
@@ -42,6 +61,35 @@ TEST_CASE("GCell Graph Test", "[routing][gcell]")
         auto box = box_type{point_type{dbu{600}, dbu{575}}, point_type{dbu{700}, dbu{675}}};
         std::vector<ophidian::routing::GCell> gcells;     
         graph.intersect(gcells, box, 1);
+        CHECK(gcells.size() == 1);
         CHECK(gcells[0] == gcell);
+    }
+    SECTION("intersect box which is bigguer tha gcell"){
+        auto box = box_type{point_type{dbu{300}, dbu{400}}, point_type{dbu{900}, dbu{800}}};
+        std::vector<ophidian::routing::GCell> gcells;
+        graph.intersect(gcells, box, 1);
+        CHECK(gcells.size() == 9);
+        std::vector<ophidian::routing::GCell> expected_gcells;
+        expected_gcells.push_back(graph.gcell(1, 2, 1));
+        expected_gcells.push_back(graph.gcell(2, 2, 1));
+        expected_gcells.push_back(graph.gcell(3, 2, 1));
+        expected_gcells.push_back(graph.gcell(1, 3, 1));
+        expected_gcells.push_back(gcell); //graph.gcell(2, 3, 1)
+        expected_gcells.push_back(graph.gcell(3, 3, 1));
+        expected_gcells.push_back(graph.gcell(1, 4, 1));
+        expected_gcells.push_back(graph.gcell(2, 4, 1));
+        expected_gcells.push_back(graph.gcell(3, 4, 1));
+        CHECK(std::is_permutation(gcells.begin(), gcells.end(), expected_gcells.begin()));
+    }
+    SECTION("intersect box with gcell edge")
+    {
+        auto box = box_type{point_type{dbu{600}, dbu{500}}, point_type{dbu{700}, dbu{600}}};
+        std::vector<ophidian::routing::GCell> gcells;
+        graph.intersect(gcells, box, 1);
+        CHECK(gcells.size() == 2);
+        std::vector<ophidian::routing::GCell> expected_gcells;
+        expected_gcells.push_back(gcell); //graph.gcell(2, 3, 1)
+        expected_gcells.push_back(graph.gcell(2, 2, 1));
+        CHECK(std::is_permutation(gcells.begin(), gcells.end(), expected_gcells.begin()));
     }
 }
