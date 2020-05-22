dd57763c3d502d5dcc96f7a54f49f39ec52add55
tarberd <bernardo.mferrari@gmail.com>
2020-05-22 15:43:53 -0300

    Root CMakeLists.txt free from find_package

--- cmake/FindDEF.cmake
+++ cmake/FindDEF.cmake
@@ -1,15 +1,39 @@
-find_path(DEF_INCLUDE_DIR "defrReader.hpp" PATHS "/usr/include" )
-find_library(DEF_LIBRARY "libdef.a" PATHS "/usr/lib" )
+find_path(DEF_INCLUDE_DIR
+  NAMES
+    "defrReader.hpp"
+)
+
+find_library(DEF_LIBRARY
+  NAMES
+    "libdef.a"
+    "libdef.so"
+)
 
 include(FindPackageHandleStandardArgs)
 
-find_package_handle_standard_args(DEF DEFAULT_MSG
-    DEF_INCLUDE_DIR
+find_package_handle_standard_args(DEF
+  FOUND_VAR
+    DEF_FOUND
+  REQUIRED_VARS
     DEF_LIBRARY
+    DEF_INCLUDE_DIR
 )
 
-add_library(DEF::def STATIC IMPORTED)
-set_target_properties(DEF::def PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES ${DEF_INCLUDE_DIR}
-    IMPORTED_LOCATION ${DEF_LIBRARY}
+if(DEF_FOUND)
+  set(Lemon_LIBRARIES ${Lemon_LIBRARY})
+  set(Lemon_INCLUDE_DIRS ${Lemon_INCLUDE_DIR})
+endif()
+
+if(DEF_FOUND AND NOT TARGET DEF::def)
+  add_library(DEF::def UNKNOWN IMPORTED)
+  set_target_properties(DEF::def PROPERTIES
+      INTERFACE_INCLUDE_DIRECTORIES ${DEF_INCLUDE_DIR}
+      IMPORTED_LOCATION ${DEF_LIBRARY}
+  )
+endif()
+
+
+mark_as_advanced(
+  DEF_INCLUDE_DIR
+  DEF_LIBRARY
 )
--- cmake/FindLEF.cmake
+++ cmake/FindLEF.cmake
@@ -1,15 +1,39 @@
-find_path(LEF_INCLUDE_DIR "lefrReader.hpp" PATHS "/usr/include" )
-find_library(LEF_LIBRARY "liblef.a" PATHS "/usr/lib" )
+find_path(LEF_INCLUDE_DIR
+  NAMES
+    "lefrReader.hpp"
+)
+
+find_library(LEF_LIBRARY
+  NAMES
+    "liblef.a"
+    "liblef.so"
+)
 
 include(FindPackageHandleStandardArgs)
 
-find_package_handle_standard_args(LEF DEFAULT_MSG
-    LEF_INCLUDE_DIR
+find_package_handle_standard_args(LEF
+  FOUND_VAR
+    LEF_FOUND
+  REQUIRED_VARS
     LEF_LIBRARY
+    LEF_INCLUDE_DIR
 )
 
-add_library(LEF::lef STATIC IMPORTED)
-set_target_properties(LEF::lef PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES ${LEF_INCLUDE_DIR}
-    IMPORTED_LOCATION ${LEF_LIBRARY}
+if(LEF_FOUND)
+  set(Lemon_LIBRARIES ${Lemon_LIBRARY})
+  set(Lemon_INCLUDE_DIRS ${Lemon_INCLUDE_DIR})
+endif()
+
+if(LEF_FOUND AND NOT TARGET LEF::def)
+  add_library(LEF::lef UNKNOWN IMPORTED)
+  set_target_properties(LEF::lef PROPERTIES
+      INTERFACE_INCLUDE_DIRECTORIES ${LEF_INCLUDE_DIR}
+      IMPORTED_LOCATION ${LEF_LIBRARY}
+  )
+endif()
+
+
+mark_as_advanced(
+  LEF_INCLUDE_DIR
+  LEF_LIBRARY
 )
